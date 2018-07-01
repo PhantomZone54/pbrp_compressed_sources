364590148a7e1b5af08036511257a31d4e50bb80
rokibhasansagar <rokibhasansagar2014@outlook.com>
2018-07-01 21:57:28 +0000

    Renewed Edition

---
+++ README.md
@@ -0,0 +1,27 @@
+# TWRP Compressed Sources
+
+[![Project tranSkadooSH](https://img.shields.io/badge/Project%20tranSkadooSH-On%20Travis%20CI-green.svg "Go to Project tranSkadooSH")](https://github.com/rokibhasansagar/tranSkadooSH)
+![Travis Build Status](https://travis-ci.org/rokibhasansagar/twrp_compressed_sources.svg?branch=master "Build Status")
+
+![GitHub repo size in bytes](https://img.shields.io/github/repo-size/rokibhasansagar/twrp_compressed_sources.svg)
+![GitHub code size in bytes](https://img.shields.io/github/languages/code-size/rokibhasansagar/twrp_compressed_sources.svg)
+
+### Compressed Repo Folders of Custom Recovery Projects
+
+Sometimes it is hard to get the _repo files_ to build _TWRP_
+:worried:
+
+That's why I used _Regalstreak_'s Project *Skadoosh* by slightly modifying as my [_tranSkadooSH_](https://github.com/rokibhasansagar/tranSkadooSH). :smirk:
+
+But I also modified this so as to work with _Travis CI_ and release the files on GitHub [Releases](https://github.com/rokibhasansagar/twrp_compressed_sources/releases) instead of [AFH](https://www.androidfilehost.com/?w=files&flid=270268).
+
+
+## Download Here:
+
+### PitchBlack TWRP
+- [![PBTWRP81](https://img.shields.io/badge/PitchBlack%20TWRP%20(twrp%208.1)-%20Download%20-blue.svg)](https://github.com/rokibhasansagar/twrp_compressed_sources/releases/tag/untagged-0b8ec540168c7b29f9f0)
+- [![PBTWRP71](https://img.shields.io/badge/PitchBlack%20TWRP%20(twrp%207.1)-%20Download%20-blue.svg)](https://github.com/rokibhasansagar/twrp_compressed_sources/releases/tag/untagged-e606f624622e4541f719)
+- [![PBTWRP60](https://img.shields.io/badge/PitchBlack%20TWRP%20(twrp%206.0)-%20Download%20-blue.svg)](https://github.com/rokibhasansagar/twrp_compressed_sources/releases/tag/untagged-3b032ac5d416365ac9ca)
+
+### Omni TWRP
+- [![OMNITWRP](https://img.shields.io/badge/Omni%20TWRP%20(6.0%20and%207.1)-Download-blue.svg)](https://github.com/rokibhasansagar/twrp_compressed_sources/releases/tag/untagged-b8c984918d3f8d824307)
---
+++ magic.sh
@@ -0,0 +1,155 @@
+#!/bin/bash
+
+# Authors - Neil "regalstreak" Agarwal, Harsh "MSF Jarvis" Shandilya, Tarang "DigiGoon" Kagathara
+# 2017
+# -----------------------------------------------------
+# Modified by - Rokib Hasan Sagar @rokibhasansagar
+# To be used via Travis CI to Release on GitHub/AFH
+# -----------------------------------------------------
+
+# Definitions
+DIR=$(pwd)
+RecName=$1
+LINK=$2
+BRANCH=$3
+GitHubMail=$4
+GitHubName=$5
+FTPHost=$6
+FTPUser=$7
+FTPPass=$8
+
+# Colors
+CL_XOS="\033[34;1m"
+CL_PFX="\033[33m"
+CL_INS="\033[36m"
+CL_RED="\033[31m"
+CL_GRN="\033[32m"
+CL_YLW="\033[33m"
+CL_BLU="\033[34m"
+CL_MAG="\033[35m"
+CL_CYN="\033[36m"
+CL_RST="\033[0m"
+
+# Get the latest repo
+PATH=~/bin:$PATH
+curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
+chmod a+x ~/bin/repo
+
+# Github Authorization
+git config --global user.email $GitHubMail
+git config --global user.name $GitHubName
+git config --global color.ui true
+
+
+# Main Function
+doSync(){
+    cd $DIR; mkdir -p $RecName/shallow; cd $RecName/shallow
+
+    # Initialize the repo data fetching
+    repo init -q -u $LINK -b $BRANCH --depth 1
+
+    # Sync it up!
+    time repo sync -c -f -q --force-sync --no-clone-bundle --no-tags -j32
+
+    echo -e $CL_RED"SHALLOW Source Syncing done"$CL_RST
+
+    cd $DIR/$RecName/
+	
+    mkdir $RecName-$BRANCH-repo-$(date +%Y%m%d)
+    
+    mv shallow/.repo/ $RecName-$BRANCH-repo-$(date +%Y%m%d)
+
+    # Show Total Sizes of the .repo folder and non-repo files
+    echo -en $CL_GRN"The total size of the consolidated .repo is ---  "$CL_RST
+    du -sh $RecName-$BRANCH-repo-$(date +%Y%m%d)
+    DDF=$(du -sh -BM $RecName-$BRANCH-repo-$(date +%Y%m%d) | awk '{print $1}' | sed 's/M//')
+    echo -en $CL_GRN"The total size of the checked-out files are ---  "$CL_RST
+    du -sh shallow
+
+    # Remove the unnecessary uncompressed checked-out files
+    rm -rf shallow
+
+    # Compress .repo folder in one piece
+    echo -e $CL_RED"Compressing files ---  "$CL_RST
+    
+    export XZ_OPT=-9e
+    
+    if [ $DDF -gt 1536 ]; then
+      mkdir repoparts
+      echo -e "Compressing and Making 1GB parts \nBe Patient..."
+      time tar -I pxz -cf - $RecName-$BRANCH-repo-$(date +%Y%m%d)/ | split -b 1024M - repoparts/$RecName-$BRANCH-repo-$(date +%Y%m%d).tar.xz.
+      SHALLOW="repoparts/$RecName-$BRANCH-repo*"
+      # Show Total Sizes of the compressed .repo
+      echo -en $CL_BLU"Final Compressed size of the consolidated .repo is ---  "$CL_RST
+      du -sh repoparts
+    else
+      echo "Compressing into tar.xz file"
+      time tar -I pxz -cf $RecName-$BRANCH-repo-$(date +%Y%m%d).tar.xz $RecName-$BRANCH-repo-$(date +%Y%m%d)/
+      SHALLOW=$RecName-$BRANCH-repo-$(date +%Y%m%d).tar.xz
+      # Show Total Sizes of the compressed .repo
+      echo -en $CL_BLU"Final Compressed size of the consolidated .repo is ---  "$CL_RST
+      du -sh $SHALLOW
+    fi
+
+    # Basic Cleanup
+    rm -rf $RecName-$BRANCH-repo-$(date +%Y%m%d)/
+
+    echo -e $CL_RED" SHALLOW Source Compression Done "$CL_RST
+
+    sortSyncedParts
+    Upload2FTP
+
+    cd $DIR/$RecName
+
+    echo -e $CL_RED"\nCongratulations! Job Done!"$CL_RST
+
+}
+
+sortSyncedParts(){
+
+    echo -e $CL_RED" SHALLOW Source  .. - ..  Begin to sort "$CL_RST
+
+    cd $DIR/$RecName
+    rm -rf upload
+    mkdir -p upload/$RecName/$BRANCH
+
+    mv $SHALLOW upload/$RecName/$BRANCH/
+
+    echo -e $CL_PFX" Done sorting "$CL_RST
+
+    # Md5s
+    echo -e $CL_PFX" Taking md5sums "$CL_RST
+
+    cd $DIR/$RecName/upload/$RecName/$BRANCH
+    md5sum * > $RecName-$BRANCH-repo-$(date +%Y%m%d).md5sum
+
+}
+
+Upload2FTP(){
+
+    echo -e $CL_XOS" Begin to upload "$CL_RST
+
+    cd $DIR/$RecName/upload
+    
+    # Upload
+    SHALLOWUP="$RecName/$BRANCH/$RecName-$BRANCH-repo*"
+    wput $SHALLOWUP ftp://"$FTPUser":"$FTPPass"@"$FTPHost"/
+
+    echo -e $CL_XOS" Done uploading "$CL_RST
+
+}
+
+# Do All The Stuff
+doallstuff(){
+
+    # Compress shallow source
+    doSync
+
+}
+
+
+# So at last do everything
+doallstuff
+if [ $? -eq 0 ]; then
+    echo -e $CL_GRN" Everything done! "$CL_RST
+fi
